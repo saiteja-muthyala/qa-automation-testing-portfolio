55f8c0d211cd79345a57dc35b04e81f59a74d47f
MUTHYALA SAITEJA <131940901+saiteja-muthyala@users.noreply.github.com>
2026-02-13 13:13:52 +0530

    Update LoginPage.java

--- selenium-automation/src/main/java/pages/LoginPage.java
+++ selenium-automation/src/main/java/pages/LoginPage.java
@@ -1,1 +1,36 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    By username = By.id("user-name");
+    By password = By.id("password");
+    By loginBtn = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String user) {
+        driver.findElement(username).sendKeys(user);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(password).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginBtn).click();
+    }
+
+    public void login(String user, String pass) {
+        enterUsername(user);
+        enterPassword(pass);
+        clickLogin();
+    }
+}
 
