af84f49a67f0dc3234f00e21f928ef3e5e3f4547
MUTHYALA SAITEJA <131940901+saiteja-muthyala@users.noreply.github.com>
2026-02-13 13:14:15 +0530

    Update LoginTest.java

--- selenium-automation/src/test/java/tests/LoginTest.java
+++ selenium-automation/src/test/java/tests/LoginTest.java
@@ -1,1 +1,33 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.LoginPage;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void validLoginTest() {
+        LoginPage login = new LoginPage(driver);
+        login.login("standard_user", "secret_sauce");
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("inventory"));
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+}
 
